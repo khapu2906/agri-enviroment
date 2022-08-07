fce3877c2929e948d654c79d12d281602fd1412b
khanh2906 <khanhphungdai@gmail.com>
2022-08-07 10:43:19 +0700

    update server enviroment

--- server/node10-install.sh
+++ server/node10-install.sh
@@ -1,13 +1,14 @@
 
 # curl -sL https://rpm.nodesource.com/setup_10.x | bash - |
 yum clean all && yum makecache fast &&
-yum install -y gcc-c++ make &&
 # yum install -y nodejs &
-yum install curl -y &&
+
 curl -sL https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash &&
+
 export NVM_DIR="$HOME/.nvm" &&
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
+
 nvm ls-remote &&
 nvm install 10.16.3 &&
 nvm use 10 &&
